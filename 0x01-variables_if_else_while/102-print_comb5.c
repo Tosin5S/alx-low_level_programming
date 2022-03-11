8dfbfe4515918f66abec7fc8ac6412414694e709
Tosin5S <olyguy.aq@gmail.com>
2022-03-11 12:53:26 -0800

    a fix: betty coding style

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int num1, num2;
+
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -17,10 +18,10 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 			if (num1 == 98 && num2 == 99)
-                                continue;
-		 	putchar(',');
+				continue;
+			putchar(',');
 			putchar(' ');
-		}   
+		}
 	}    
 	putchar('\n');
 	return (0);
