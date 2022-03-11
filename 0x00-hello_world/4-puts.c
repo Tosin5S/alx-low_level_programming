3d510cb143df2b18b220aeda85980b444654e5be
Tosin5S <olyguy.aq@gmail.com>
2022-03-10 17:46:04 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * imain - This uses program prints exactly the string
+ * main - This uses program prints exactly the string
  * "Programming is like building a multilingual puzzle
  * followed by a new line.
  * Return: always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
